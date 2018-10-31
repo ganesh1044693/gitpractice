29793b84397f0b56bfce146c813c785eb616fe78
Ganesh Kamma <M1044693@mindtree.com>
2018-10-31 15:16:07 +0530

    added logging facility

--- SpringBootRest/src/main/java/com/javainterviewpoint/StudentController.java
+++ SpringBootRest/src/main/java/com/javainterviewpoint/StudentController.java
@@ -2,6 +2,8 @@ package com.javainterviewpoint;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,46 +12,31 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class StudentController
-{
-    @Autowired
-    StudentRepository studentRepository;
-    //Create
-    @RequestMapping(value = "/student", method = RequestMethod.POST)
-    public Student createStudent(@RequestBody Student student)
-    {
-        return studentRepository.save(student);
-    }
-    
-    //Read
-    @RequestMapping(value="/student/{id}", method = RequestMethod.GET)
-    public Student getStudentById(@PathVariable long id)
-    {
-        return studentRepository.findOne(id);
-    }
-    
-    @RequestMapping(value="/students", method = RequestMethod.GET)
-    public List<Student> getAllStudents()
-    {
-        return (List<Student>)studentRepository.findAll();
-    }
-    
-    //Update
-    @RequestMapping(value = "/student", method = RequestMethod.PUT)
-    public Student updateStudent(@RequestBody Student student)
-    {
-        return studentRepository.save(student);
-    }
-    
-    //Delete
-    @RequestMapping(value = "/student/{id}", method = RequestMethod.DELETE)
-    public void deleteStudent(@PathVariable long id)
-    {
-        studentRepository.delete(id);
-    }
-    @RequestMapping(value="/studentss", method = RequestMethod.GET)
-    public String getA()
-    {
-        return "Hllonsic";
-    }
+public class StudentController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(StudentController.class);
+	@Autowired
+	StudentRepository studentRepository;
+
+	// Create
+	@RequestMapping(value = "/student", method = RequestMethod.POST)
+	public Student createStudent(@RequestBody Student student) {
+		return studentRepository.save(student);
+	}
+
+	// Read
+	@RequestMapping(value = "/student/{id}", method = RequestMethod.GET)
+	public Student getStudentById(@PathVariable long id) {
+		return studentRepository.findOne(id);
+	}
+
+	@RequestMapping(value = "/students", method = RequestMethod.GET)
+	public List<Student> getAllStudents() {
+		List<Student> list = null;
+		LOGGER.debug("entee into StudentController-getAllStudents() method ");
+		list = (List<Student>) studentRepository.findAll();
+		LOGGER.debug("list of students from the api "+list.toString());
+		LOGGER.debug("exit from into StudentController-getAllStudents() method ");
+		return list;
+	}
+
 }
